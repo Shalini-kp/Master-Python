1ac6a57afd07f0be4832645bd006d4e4b867d718
rsunray <sunray2@outlook.com>
2017-08-29 00:06:54 +0530

    completed intro andlexanalysis

---
+++ Butterfly/intro.py
@@ -0,0 +1,28 @@
+#-*- coding:utf8 -*-
+
+#single line comments
+
+#explicit line joining
+print("hello " #compile time literal concatenation
+	"world "  \
+	"of python" 
+	)
+
+#implicit line joining
+month_names = ['Januari', 'Februari', 'Maart',      # These are the
+               'April',   'Mei',      'Juni',       # Dutch names
+               'Juli',    'Augustus', 'September',  # for the months
+               'Oktober', 'November', 'December']   # of the year
+
+#indent and dedent stack to keep track of indentation
+"""
+Python keywords
+---------------
+False      class      finally    is         return
+None       continue   for        lambda     try
+True       def        from       nonlocal   while
+and        del        global     not        with
+as         elif       if         or         yield
+assert     else       import     pass
+break      except     in         raise
+"""
